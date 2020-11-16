e96d466f60c401d2afd8824ea296efa00c38d558
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-11-16 13:49:41 -0600

    added very basic world generation

--- classes.py
+++ classes.py
@@ -1,8 +1,50 @@
 import pygame
 from constants import *
 import random
+pygame.init()
+font = pygame.font.SysFont('Consolas', 30)
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    K_t,
+    K_f,
+    K_g,
+    K_h,
+    KEYDOWN,
+    QUIT,
+)
 
-class Player(pygame.sprite.Sprite()):
+class Player(pygame.sprite.Sprite):
     def __init__(self):
         super(Player, self).__init__()
-    
+        self.surf = pygame.Surface((25, 25))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(center=(round(SCREEN_WIDTH/2), round(SCREEN_HEIGHT/2)))
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self, pos, id):
+        super(Block, self).__init__()
+        self.id = id
+        if self.id > 0:
+            self.surf = pygame.Surface((25, 25))
+            if self.id == 1:
+                self.surf.fill((255, 255, 0))
+            elif self.id == 2:
+                self.surf.fill((0, 255, 0))
+            self.rect = self.surf.get_rect(center=pos)
+        else:
+            self.surf = pygame.Surface((0, 0))
+            self.surf.fill((255, 255, 0))
+            self.rect = self.surf.get_rect(center=pos)
+            self.id = 0
+        self.g_position = pos
+
+class Chunk():
+    def __init__(self, pos):
+        self.blocks = []
+        for y in range(CHUNK_SIZE):
+            for x in range(CHUNK_SIZE):
+                self.blocks.append(Block((x*25, y*25), random.randint(-10, 2)))
--- main.py
+++ main.py
@@ -1,1 +1,49 @@
 from classes import *
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+player = Player()
+
+all_sprites = pygame.sprite.Group()
+chunk_one = Chunk((-2000, -2000))
+for block in chunk_one.blocks:
+    all_sprites.add(block)
+running = True
+while running:
+    screen.fill((0, 0, 0))
+    pressed_keys = pygame.key.get_pressed()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            running = False
+    screen.blit(player.surf, player.rect)
+    speed = -1
+    hit_id = []
+    for sprite in all_sprites:
+        if pressed_keys[K_UP]:
+            sprite.rect.move_ip(0, -speed)
+            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+                hit_id.append(1)
+        if pressed_keys[K_DOWN]:
+            sprite.rect.move_ip(0, speed)
+            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+                hit_id.append(2)
+        if pressed_keys[K_LEFT]:
+            sprite.rect.move_ip(-speed, 0)
+            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+                hit_id.append(3)
+        if pressed_keys[K_RIGHT]:
+            sprite.rect.move_ip(speed, 0)
+            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+                hit_id.append(4)
+    for sprite in all_sprites:
+        if 1 in hit_id:
+            sprite.rect.move_ip(0, speed)
+        if 2 in hit_id:
+            sprite.rect.move_ip(0, -speed)
+        if 3 in hit_id:
+            sprite.rect.move_ip(speed, 0)
+        if 4 in hit_id:
+            sprite.rect.move_ip(-speed, 0)
+        screen.blit(sprite.surf, sprite.rect)
+    pygame.display.flip()
+    clock.tick(FRAME_RATE)
+    
