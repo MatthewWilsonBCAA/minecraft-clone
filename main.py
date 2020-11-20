e965fd4feed6ee0fe9d59cf609889d964a8d42a6
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-11-20 10:11:49 -0600

    fixed it so that player cannot build on top of themselves

--- main.py
+++ main.py
@@ -123,9 +123,7 @@ while running:
             if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
                 hit_id.append(4)
         if sprite.rect.collidepoint(x, y) and hasattr(sprite, "id"):
-            r = abs(player.rect.x - x)
-            s = abs(player.rect.y - y)
-            if r < 100 and r > 25 and s < 100 and s > 25:
+            if not pygame.sprite.collide_rect(player, sprite):
                 if sprite.id != 0 and is_build == False:
                     sprite.hp -= player.pickaxe
                     if sprite.hp <= 0:
