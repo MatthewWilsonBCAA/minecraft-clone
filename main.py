d4adc7fd8cdd90abb316a43ee97c215bf8d956d7
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-11-23 14:11:21 -0600

    possibly? idk

--- main.py
+++ main.py
@@ -37,7 +37,23 @@ pos_list = [
     (-800+x_pos, -800+y_pos),
     (0+x_pos, -800+y_pos),
     (800+x_pos, -800+y_pos),
-    (-800+x_pos, 800+y_pos)
+    (-800+x_pos, 800+y_pos),
+    (-1600+x_pos, -1600+y_pos),
+    (-1600+x_pos, -800+y_pos),
+    (-800+x_pos, -1600+y_pos),
+    (-1600+x_pos, 0+y_pos),
+    (0+x_pos, -1600+y_pos),
+    (-1600+x_pos, 800+y_pos),
+    (800+x_pos, -1600+y_pos),
+    (-1600+x_pos, 1600+y_pos),
+    (1600+x_pos, 1600+y_pos),
+    (-800+x_pos, 1600+y_pos),
+    (1600+x_pos, -800+y_pos),
+    (0+x_pos, 1600+y_pos),
+    (1600+x_pos, 0+y_pos),
+    (800+x_pos, 1600+y_pos),
+    (1600+x_pos, 800+y_pos),
+    (1600+x_pos, 1600+y_pos)
 ]
 try:
     with open("world.json", "r") as file:
@@ -51,6 +67,22 @@ try:
         chunk_seven = Chunk(pos_list[6], False, 'seven', data['seven'])
         chunk_eight = Chunk(pos_list[7], False, 'eight', data['eight'])
         chunk_nine = Chunk(pos_list[8], False, 'nine', data['nine'])
+        chunk_ten = Chunk(pos_list[9], False, 'ten', data['ten'])
+        chunk_eleven = Chunk(pos_list[10], False, 'eleven', data['eleven'])
+        chunk_twelve = Chunk(pos_list[11], False, 'twelve', data['twelve'])
+        chunk_thirteen = Chunk(pos_list[12], False, 'thirteen', data['thirteen'])
+        chunk_fourteen = Chunk(pos_list[13], False, 'fourteen', data['fourteen'])
+        chunk_fifteen = Chunk(pos_list[14], False, 'fifteen', data['fifteen'])
+        chunk_sixteen = Chunk(pos_list[15], False, 'sixteen', data['sixteen'])
+        chunk_seventeen = Chunk(pos_list[16], False, 'seventeen', data['seventeen'])
+        chunk_eighteen = Chunk(pos_list[17], False, 'eighteen', data['eighteen'])
+        chunk_nineteen = Chunk(pos_list[18], False, 'nineteen', data['nineteen'])
+        chunk_twenty = Chunk(pos_list[19], False, 'twenty', data['twenty'])
+        chunk_twentyone = Chunk(pos_list[20], False, 'twentyone', data['twentyone'])
+        chunk_twentytwo = Chunk(pos_list[21], False, 'twentytwo', data['twentytwo'])
+        chunk_twentythree = Chunk(pos_list[22], False, 'twentythree', data['twentythree'])
+        chunk_twentyfour = Chunk(pos_list[23], False, 'twentyfour', data['twentyfour'])
+        chunk_twentyfive = Chunk(pos_list[24], False, 'twentyfive', data['twentyfive'])
 except:
     chunk_one = Chunk(pos_list[0], True, 'one')
     chunk_two = Chunk(pos_list[1], False, 'two')
@@ -61,9 +93,51 @@ except:
     chunk_seven = Chunk(pos_list[6], False, 'seven')
     chunk_eight = Chunk(pos_list[7], False, 'eight')
     chunk_nine = Chunk(pos_list[8], False, 'nine')
+    chunk_ten = Chunk(pos_list[0], False, 'ten')
+    chunk_eleven = Chunk(pos_list[1], False, 'eleven')
+    chunk_twelve = Chunk(pos_list[2], False, 'twelve')
+    chunk_thirteen = Chunk(pos_list[3], False, 'thirteen')
+    chunk_fourteen = Chunk(pos_list[4], False, 'fourteen')
+    chunk_fifteen = Chunk(pos_list[14], False, 'fifteen')
+    chunk_sixteen = Chunk(pos_list[15], False, 'sixteen')
+    chunk_seventeen = Chunk(pos_list[16], False, 'seventeen')
+    chunk_eighteen = Chunk(pos_list[17], False, 'eighteen')
+    chunk_nineteen = Chunk(pos_list[18], False, 'nineteen')
+    chunk_twenty = Chunk(pos_list[19], False, 'twenty')
+    chunk_twentyone = Chunk(pos_list[20], False, 'twentyone')
+    chunk_twentytwo = Chunk(pos_list[21], False, 'twentytwo')
+    chunk_twentythree = Chunk(pos_list[22], False, 'twentythree')
+    chunk_twentyfour = Chunk(pos_list[23], False, 'twentyfour')
+    chunk_twentyfive = Chunk(pos_list[24], False, 'twentyfive')
 all_sprites = pygame.sprite.Group()
 
-chunk_list = [chunk_one, chunk_two, chunk_three, chunk_four, chunk_five, chunk_six, chunk_seven, chunk_eight, chunk_nine]
+chunk_list = [
+    chunk_one,
+    chunk_two,
+    chunk_three,
+    chunk_four,
+    chunk_five,
+    chunk_six,
+    chunk_seven,
+    chunk_eight,
+    chunk_nine,
+    chunk_ten,
+    chunk_eleven,
+    chunk_twelve,
+    chunk_thirteen,
+    chunk_fourteen,
+    chunk_fifteen,
+    chunk_sixteen,
+    chunk_seventeen,
+    chunk_eighteen,
+    chunk_nineteen,
+    chunk_twenty,
+    chunk_twentyone,
+    chunk_twentytwo,
+    chunk_twentythree,
+    chunk_twentyfour,
+    chunk_twentyfive
+    ]
 for chunk in chunk_list:
     for block in chunk.blocks:
         all_sprites.add(block)
@@ -78,6 +152,7 @@ x = 0
 y = 0
 is_build = False
 selected_block = 0
+cam = pygame.Vector2((0, 0))
 
 while running:
     screen.fill((0, 0, 0))
@@ -108,55 +183,59 @@ while running:
     speed = -2
     hit_id = []
     rend_r = []
-    for sprite in all_sprites:
-        if pressed_keys[K_w]:
-            sprite.rect.move_ip(0, -speed)
-            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
-                hit_id.append(1)
-        if pressed_keys[K_s]:
-            sprite.rect.move_ip(0, speed)
-            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
-                hit_id.append(2)
-        if pressed_keys[K_a]:
-            sprite.rect.move_ip(-speed, 0)
-            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
-                hit_id.append(3)
-        if pressed_keys[K_d]:
-            sprite.rect.move_ip(speed, 0)
-            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
-                hit_id.append(4)
-        if sprite.rect.collidepoint(x, y) and hasattr(sprite, "id") and not hasattr(sprite, "name"):
-            dist = ((player.rect.x - x) ** 2 + (player.rect.y - y)  ** 2) ** 0.5
-            if not pygame.sprite.collide_rect(player, sprite) and dist < 125:
-                if sprite.id != 0 and is_build == False:
-                    sprite.hp -= player.pickaxe
-                    if sprite.hp <= 0:
-                        player.add_item(sprite.id, 1)
-                        sprite.change_block(0)
-                elif sprite.id == 0 and is_build == True and selected_block != 0 and player.inventory[str(selected_block)] > 0:
-                    sprite.change_block(selected_block)
-                    player.remove_item(selected_block, 1)
+    if pressed_keys[K_w]:
+        player.rect.move_ip(0, -speed)
+    if pressed_keys[K_s]:
+        player.rect.move_ip(0, speed)
+    if pressed_keys[K_a]:
+        player.rect.move_ip(-speed, 0)
+    if pressed_keys[K_d]:
+        player.rect.move_ip(speed, 0)
+    camera_move = pygame.Vector2()
+    if pressed_keys[K_w]: camera_move += (0, -speed)
+    if pressed_keys[K_s]: camera_move += (0, speed)
+    if pressed_keys[K_a]: camera_move += (-speed, -0)
+    if pressed_keys[K_d]: camera_move += (speed, 0)
+    if camera_move.length() > 0: camera_move.normalize_ip()
+    cam += camera_move
+    
     prev_sprite = False
     next_sprite = False
     ver_sprite = False
     bot_sprite = False
+    
     for chunk in chunk_list:
         # three extra vars for tracking what block we are on
         z = 0
         v = 0
         b = 0
         for sprite in chunk.blocks:
-            if 1 in hit_id:
-                sprite.rect.move_ip(0, speed)
-            if 2 in hit_id:
-                sprite.rect.move_ip(0, -speed)
-            if 3 in hit_id:
-                sprite.rect.move_ip(speed, 0)
-            if 4 in hit_id:
-                sprite.rect.move_ip(-speed, 0)
-            if sprite.rect.x > -20 and sprite.rect.x < SCREEN_WIDTH + 20 and sprite.rect.y > -20 and sprite.rect.y < SCREEN_HEIGHT + 20:
-                if prev_sprite and sprite.check_render(prev_sprite, next_sprite, ver_sprite, bot_sprite):
-                    screen.blit(sprite.surf, sprite.rect)
+            if pressed_keys[K_w]:
+                if sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+                    hit_id.append(1)
+            if pressed_keys[K_s]:
+                if sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+                    hit_id.append(2)
+            if pressed_keys[K_a]:
+                if sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+                    hit_id.append(3)
+            if pressed_keys[K_d]:
+                if sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+                    hit_id.append(4)
+            if sprite.rect.collidepoint(x, y) and hasattr(sprite, "id") and not hasattr(sprite, "name"):
+                dist = ((player.rect.x - x) ** 2 + (player.rect.y - y)  ** 2) ** 0.5
+                if not pygame.sprite.collide_rect(player, sprite) and dist < 125:
+                    if sprite.id != 0 and is_build == False:
+                        sprite.hp -= player.pickaxe
+                        if sprite.hp <= 0:
+                            player.add_item(sprite.id, 1)
+                            sprite.change_block(0)
+                    elif sprite.id == 0 and is_build == True and selected_block != 0 and player.inventory[str(selected_block)] > 0:
+                        sprite.change_block(selected_block)
+                        player.remove_item(selected_block, 1)
+            #if sprite.rect.x > -20 and sprite.rect.x < SCREEN_WIDTH + 20 and sprite.rect.y > -20 and sprite.rect.y < SCREEN_HEIGHT + 20:
+            if prev_sprite and sprite.check_render(prev_sprite, next_sprite, ver_sprite, bot_sprite):
+                screen.blit(sprite.surf, sprite.rect.move(*cam))
             prev_sprite = sprite
             if z < len(chunk.blocks) - 2:
                 next_sprite = chunk.blocks[z + 2]
@@ -171,6 +250,14 @@ while running:
             z += 1
             v += 1
             b += 1
+    if 1 in hit_id:
+        player.rect.move_ip(0, speed)
+    if 2 in hit_id:
+        player.rect.move_ip(0, -speed)
+    if 3 in hit_id:
+        player.rect.move_ip(speed, 0)
+    if 4 in hit_id:
+        player.rect.move_ip(-speed, 0)
                 
     
     for sprite in ui_group:
@@ -213,8 +300,8 @@ def split(a, n):
     return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
 with open('stats.json', 'w') as file:
-    x_pos = chunk_one.blocks[0].rect.x + 12
-    y_pos = chunk_one.blocks[0].rect.y + 12
+    x_pos = cam.rect.x + 12
+    y_pos = cam.rect.y + 12
     data = {'hp': player.hp, 'pickaxe': player.pickaxe, 'axe': player.axe, 'sword': player.sword, 'x_pos': x_pos, 'y_pos': y_pos}
     json.dump(data, file)
 
@@ -224,9 +311,9 @@ with open('inventory.json', 'w') as file:
 
 
 with open('world.json', 'w') as file:
-    keys = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    keys = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty', 'twentyone', 'twentytwo', 'twentythree', 'twentyfour', 'twentyfive']
     z = 0
-    data = {'one': [], 'two': [], 'three': [], 'four': [], 'five': [], 'six': [], 'seven': [], 'eight': [], 'nine': []}
+    data = {'one': [], 'two': [], 'three': [], 'four': [], 'five': [], 'six': [], 'seven': [], 'eight': [], 'nine': [], 'ten': [], 'eleven': [], 'twelve': [], 'thirteen': [], 'fourteen': [], 'fifteen': [], 'sixteen': [], 'seventeen': [], 'eighteen': [], 'nineteen': [], 'twenty': [], 'twentyone': [], 'twentytwo': [], 'twentythree': [], 'twentyfour': [], 'twentyfive': []}
     for chunk in chunk_list:
         rows = list(split(chunk.blocks, 32))
         for row in rows:
