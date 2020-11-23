c0e1df236e179b3d22cd7c63c3004c80da90a68e
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-11-23 15:45:01 -0600

    adjusted UI

--- constants.py
+++ constants.py
@@ -18,7 +18,7 @@ BLOCK_LIST = [
 #0, just background
 #1, upgrade pickaxe
 UI_LIST = [
-    [(200, 160), (100, 80), 2],
+    [(200, 180), (100, 90), 2],
     [(220, 100), (SCREEN_WIDTH - 110, 50), 0],
     [(50, 50), (SCREEN_WIDTH - 175, 75), 1]
 ]
