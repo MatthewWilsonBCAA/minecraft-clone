a79d5973395eda03be169c43f6d46951d502c599
Matthew Wilson <mwilson21@basecampcodingacademy.org>
2020-12-04 10:45:33 -0600

    some shadows

--- classes.py
+++ classes.py
@@ -2,9 +2,10 @@ import pygame
 from constants import *
 import random
 from chunks import chunk_styles, files
+
 pygame.init()
-font = pygame.font.SysFont('Consolas', 20)
-small_font = pygame.font.SysFont('Consolas', 13)
+font = pygame.font.SysFont("Consolas", 20)
+small_font = pygame.font.SysFont("Consolas", 13)
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -26,14 +27,16 @@ from pygame.locals import (
     K_d,
     KEYDOWN,
     QUIT,
-    MOUSEBUTTONDOWN
+    MOUSEBUTTONDOWN,
 )
+
 
 class Light(pygame.sprite.Sprite):
     def __init__(self, size, pos):
         super(Light, self).__init__()
         self.circle = pygame.Circle(size)
         self.rect = self.circle.get_rect(center=pos)
+
 
 class UI(pygame.sprite.Sprite):
     def __init__(self, size, pos, type):
@@ -51,6 +54,7 @@ class UI(pygame.sprite.Sprite):
             for block in BLOCK_LIST:
                 self.blocks.append(BlockImage(block[0], block[1], block[2], block[3]))
 
+
 class BlockImage(pygame.sprite.Sprite):
     def __init__(self, name, image, pos, id):
         self.name = name
@@ -59,28 +63,35 @@ class BlockImage(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect(center=pos)
 
     def set_block(self):
-        return self.id 
-    
-        
+        return self.id
+
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, inventory, hp, pickaxe, axe, sword):
         super(Player, self).__init__()
         self.surf = pygame.Surface((20, 20))
         self.surf.fill((255, 255, 255))
-        self.rect = self.surf.get_rect(center=(round(SCREEN_WIDTH/2), round(SCREEN_HEIGHT/2)))
+        self.rect = self.surf.get_rect(
+            center=(round(SCREEN_WIDTH / 2), round(SCREEN_HEIGHT / 2))
+        )
         self.inventory = inventory
         self.hp = hp
         self.pickaxe = pickaxe
         self.axe = axe
         self.sword = sword
+
     def add_item(self, item_id, amount):
         self.inventory[str(item_id)] += amount
+
     def remove_item(self, item_id, amount):
         self.inventory[str(item_id)] -= amount
+
     def upgrade_pick(self):
-        if self.inventory['4'] >= self.pickaxe - UPGRADE_COST:
-            self.inventory['4'] -= self.pickaxe - UPGRADE_COST
+        if self.inventory["4"] >= self.pickaxe - UPGRADE_COST:
+            self.inventory["4"] -= self.pickaxe - UPGRADE_COST
             self.pickaxe += 1
+
+
 class Block(pygame.sprite.Sprite):
     def __init__(self, pos, id):
         super(Block, self).__init__()
@@ -96,21 +107,24 @@ class Block(pygame.sprite.Sprite):
         self.g_position = pos
         self.rend = 0
         self.a = 255
+
     def change_block(self, id):
         self.id = id
         if self.id != 0:
-            self.surf = pygame.image.load(BLOCK_LIST[id-1][1]).convert()
-            self.hp = BLOCK_LIST[id-1][3]
+            self.surf = pygame.image.load(BLOCK_LIST[id - 1][1]).convert()
+            self.hp = BLOCK_LIST[id - 1][3]
         else:
             self.surf = pygame.image.load("images/ground.png").convert()
             self.hp = 0
+
     def check_render(self, dist):
         x = set()
         for d in dist:
             x.add(int(d[1] / 3 - d[0]) + 150)
         return max(x)
 
-class Chunk():
+
+class Chunk:
     def __init__(self, pos, spawn_chunk, name, data=None):
         self.blocks = []
         self.name = name
@@ -118,15 +132,27 @@ class Chunk():
             self.chunk_type = files[0]
             self.mirrored_chance = 1
         else:
-            self.chunk_type = files[random.randint(0, len(files)-1)]
+            self.chunk_type = files[random.randint(0, len(files) - 1)]
             self.mirrored_chance = random.randint(1, 2)
         for y in range(CHUNK_SIZE):
             for x in range(CHUNK_SIZE):
                 # print(str(x) + ", " + str(y))
                 if data:
-                    self.blocks.append(Block((x*25 + pos[0] , y*25 + pos[1]), data[y][x]))
+                    self.blocks.append(
+                        Block((x * 25 + pos[0], y * 25 + pos[1]), data[y][x])
+                    )
                 else:
                     if self.mirrored_chance == 1:
-                        self.blocks.append(Block((x*25 + pos[0] , y*25 + pos[1]), chunk_styles[self.chunk_type][y][x]))
+                        self.blocks.append(
+                            Block(
+                                (x * 25 + pos[0], y * 25 + pos[1]),
+                                chunk_styles[self.chunk_type][y][x],
+                            )
+                        )
                     else:
-                        self.blocks.append(Block((x*25 + pos[0] , y*25 + pos[1]), chunk_styles[self.chunk_type][x][y]))
+                        self.blocks.append(
+                            Block(
+                                (x * 25 + pos[0], y * 25 + pos[1]),
+                                chunk_styles[self.chunk_type][x][y],
+                            )
+                        )
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from classes import *
-from itertools import islice 
+from itertools import islice
 import json
+
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Matt's Top Down Minecraft")
@@ -9,12 +10,12 @@ pygame.display.set_icon(icon)
 try:
     with open("stats.json", "r") as file:
         data = json.load(file)
-        hp = data['hp']
-        pickaxe = data['pickaxe']
-        axe = data['axe']
-        sword = data['sword']
-        x_pos = data['x_pos']
-        y_pos = data['y_pos']
+        hp = data["hp"]
+        pickaxe = data["pickaxe"]
+        axe = data["axe"]
+        sword = data["sword"]
+        x_pos = data["x_pos"]
+        y_pos = data["y_pos"]
 except:
     hp = 10
     pickaxe = 10
@@ -26,44 +27,54 @@ try:
     with open("inventory.json", "r") as file:
         inven = json.load(file)
 except:
-    inven = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0}
+    inven = {"1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0}
 player = Player(inven, hp, pickaxe, axe, sword)
 pos_list = [
-    (0+x_pos, 0+y_pos),
-    (800+x_pos, 0+y_pos),
-    (0+x_pos, 800+y_pos),
-    (800+x_pos, 800+y_pos),
-    (-800+x_pos, 0+y_pos),
-    (-800+x_pos, -800+y_pos),
-    (0+x_pos, -800+y_pos),
-    (800+x_pos, -800+y_pos),
-    (-800+x_pos, 800+y_pos)
+    (0 + x_pos, 0 + y_pos),
+    (800 + x_pos, 0 + y_pos),
+    (0 + x_pos, 800 + y_pos),
+    (800 + x_pos, 800 + y_pos),
+    (-800 + x_pos, 0 + y_pos),
+    (-800 + x_pos, -800 + y_pos),
+    (0 + x_pos, -800 + y_pos),
+    (800 + x_pos, -800 + y_pos),
+    (-800 + x_pos, 800 + y_pos),
 ]
 try:
     with open("world.json", "r") as file:
         data = json.load(file)
-        chunk_one = Chunk(pos_list[0], True, 'one', data['one'])
-        chunk_two = Chunk(pos_list[1], False, 'two', data['two'])
-        chunk_three = Chunk(pos_list[2], False, 'three', data['three'])
-        chunk_four = Chunk(pos_list[3], False, 'four', data['four'])
-        chunk_five = Chunk(pos_list[4], False, 'five', data['five'])
-        chunk_six = Chunk(pos_list[5], False, 'six', data['six'])
-        chunk_seven = Chunk(pos_list[6], False, 'seven', data['seven'])
-        chunk_eight = Chunk(pos_list[7], False, 'eight', data['eight'])
-        chunk_nine = Chunk(pos_list[8], False, 'nine', data['nine'])
+        chunk_one = Chunk(pos_list[0], True, "one", data["one"])
+        chunk_two = Chunk(pos_list[1], False, "two", data["two"])
+        chunk_three = Chunk(pos_list[2], False, "three", data["three"])
+        chunk_four = Chunk(pos_list[3], False, "four", data["four"])
+        chunk_five = Chunk(pos_list[4], False, "five", data["five"])
+        chunk_six = Chunk(pos_list[5], False, "six", data["six"])
+        chunk_seven = Chunk(pos_list[6], False, "seven", data["seven"])
+        chunk_eight = Chunk(pos_list[7], False, "eight", data["eight"])
+        chunk_nine = Chunk(pos_list[8], False, "nine", data["nine"])
 except:
-    chunk_one = Chunk(pos_list[0], True, 'one')
-    chunk_two = Chunk(pos_list[1], False, 'two')
-    chunk_three = Chunk(pos_list[2], False, 'three')
-    chunk_four = Chunk(pos_list[3], False, 'four')
-    chunk_five = Chunk(pos_list[4], False, 'five')
-    chunk_six = Chunk(pos_list[5], False, 'six')
-    chunk_seven = Chunk(pos_list[6], False, 'seven')
-    chunk_eight = Chunk(pos_list[7], False, 'eight')
-    chunk_nine = Chunk(pos_list[8], False, 'nine')
+    chunk_one = Chunk(pos_list[0], True, "one")
+    chunk_two = Chunk(pos_list[1], False, "two")
+    chunk_three = Chunk(pos_list[2], False, "three")
+    chunk_four = Chunk(pos_list[3], False, "four")
+    chunk_five = Chunk(pos_list[4], False, "five")
+    chunk_six = Chunk(pos_list[5], False, "six")
+    chunk_seven = Chunk(pos_list[6], False, "seven")
+    chunk_eight = Chunk(pos_list[7], False, "eight")
+    chunk_nine = Chunk(pos_list[8], False, "nine")
 all_sprites = pygame.sprite.Group()
 
-chunk_list = [chunk_one, chunk_two, chunk_three, chunk_four, chunk_five, chunk_six, chunk_seven, chunk_eight, chunk_nine]
+chunk_list = [
+    chunk_one,
+    chunk_two,
+    chunk_three,
+    chunk_four,
+    chunk_five,
+    chunk_six,
+    chunk_seven,
+    chunk_eight,
+    chunk_nine,
+]
 for chunk in chunk_list:
     for block in chunk.blocks:
         all_sprites.add(block)
@@ -71,7 +82,7 @@ for chunk in chunk_list:
 ui_group = pygame.sprite.Group()
 for param in UI_LIST:
     obj = UI(param[0], param[1], param[2])
-    ui_group.add(obj)   
+    ui_group.add(obj)
 
 running = True
 x = 0
@@ -82,7 +93,7 @@ checked_light_blocks = False
 light_timer = 0
 lit_spots = []
 
-dist_b = [] #used for block lighting only
+dist_b = []  # used for block lighting only
 
 while running:
     screen.fill((0, 0, 0))
@@ -104,8 +115,8 @@ while running:
         if event.type == KEYDOWN:
             if event.key == K_b:
                 is_build = not is_build
-        if event.type == MOUSEBUTTONDOWN: 
-            x,y = event.pos
+        if event.type == MOUSEBUTTONDOWN:
+            x, y = event.pos
         else:
             x = 0
             y = 0
@@ -113,32 +124,65 @@ while running:
     speed = -2
     hit_id = []
     rend_r = []
-    #light_timer += 1
-    #if light_timer > 5:
+    # light_timer += 1
+    # if light_timer > 5:
     checked_light_blocks = False
-        #light_timer = 0
+    # light_timer = 0
     lit_spots.clear()
+    list_sprites = all_sprites.sprites()
+    list_len = len(list_sprites)
+    prev_b = 0
+    next_b = 0
+    z = 0
+
     for sprite in all_sprites:
+
         if sprite.id == 7 and not checked_light_blocks:
-            lit_spots.append((sprite.rect.x, sprite.rect.y, LIGHT_DIST))
+            p = None
+            n = None
+            if prev_b and prev_b != 7:
+                p = sprite.rect.x + 12
+            if next_b and next_b != 7:
+                n = sprite.rect.x - 12
+            lit_spots.append((sprite.rect.x, sprite.rect.y, LIGHT_DIST, p, n))
         if pressed_keys[K_w]:
             sprite.rect.move_ip(0, -speed)
-            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+            if (
+                hasattr(sprite, "id")
+                and sprite.id > 0
+                and pygame.sprite.collide_rect(player, sprite)
+            ):
                 hit_id.append(1)
         if pressed_keys[K_s]:
             sprite.rect.move_ip(0, speed)
-            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+            if (
+                hasattr(sprite, "id")
+                and sprite.id > 0
+                and pygame.sprite.collide_rect(player, sprite)
+            ):
                 hit_id.append(2)
         if pressed_keys[K_a]:
             sprite.rect.move_ip(-speed, 0)
-            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+            if (
+                hasattr(sprite, "id")
+                and sprite.id > 0
+                and pygame.sprite.collide_rect(player, sprite)
+            ):
                 hit_id.append(3)
         if pressed_keys[K_d]:
             sprite.rect.move_ip(speed, 0)
-            if hasattr(sprite, "id") and sprite.id > 0 and pygame.sprite.collide_rect(player, sprite):
+            if (
+                hasattr(sprite, "id")
+                and sprite.id > 0
+                and pygame.sprite.collide_rect(player, sprite)
+            ):
                 hit_id.append(4)
-        if sprite.rect.collidepoint(x, y) and hasattr(sprite, "id") and not hasattr(sprite, "name"):
-            dist = ((player.rect.x - x) ** 2 + (player.rect.y - y)  ** 2) ** 0.5
+        if (
+            sprite.rect.collidepoint(x, y)
+            and hasattr(sprite, "id")
+            and not hasattr(sprite, "name")
+        ):
+            dist = ((player.rect.x - x) ** 2 + (player.rect.y - y) ** 2) ** 0.5
             if not pygame.sprite.collide_rect(player, sprite) and dist < 125:
                 if sprite.id != 0 and is_build == False:
                     sprite.hp -= player.pickaxe
@@ -146,10 +190,22 @@ while running:
                         player.add_item(sprite.id, 1)
                         sprite.change_block(0)
                     checked_light_blocks = False
-                elif sprite.id == 0 and is_build == True and selected_block != 0 and player.inventory[str(selected_block)] > 0:
+                elif (
+                    sprite.id == 0
+                    and is_build == True
+                    and selected_block != 0
+                    and player.inventory[str(selected_block)] > 0
+                ):
                     checked_light_blocks = False
                     sprite.change_block(selected_block)
                     player.remove_item(selected_block, 1)
+        prev_b = sprite.id
+        if z < list_len - 2:
+            next_b = list_sprites[z + 2].id
+        z += 1
+
+    z = 0
+
     for sprite in all_sprites:
         if 1 in hit_id:
             sprite.rect.move_ip(0, speed)
@@ -159,26 +215,54 @@ while running:
             sprite.rect.move_ip(speed, 0)
         if 4 in hit_id:
             sprite.rect.move_ip(-speed, 0)
-            
-        
-        if sprite.rect.x > 0 and sprite.rect.x < SCREEN_WIDTH and sprite.rect.y > 0 and sprite.rect.y < SCREEN_HEIGHT:
+
+        if (
+            sprite.rect.x > 0
+            and sprite.rect.x < SCREEN_WIDTH
+            and sprite.rect.y > 0
+            and sprite.rect.y < SCREEN_HEIGHT
+        ):
             if sprite.id == 7:
                 screen.blit(sprite.surf, sprite.rect)
             else:
                 dist_b.clear()
-                dist_b.append(((((player.rect.x - sprite.rect.x) ** 2 + (player.rect.y - sprite.rect.y)  ** 2) ** 0.5), REND_DIST))
+                dist_b.append(
+                    [
+                        (
+                            (
+                                (player.rect.x - sprite.rect.x) ** 2
+                                + (player.rect.y - sprite.rect.y) ** 2
+                            )
+                            ** 0.5
+                        ),
+                        REND_DIST,
+                    ]
+                )
                 for cor in lit_spots:
+                    if cor[3] != None and sprite.rect.x < cor[3]:
+                        continue
+                    if cor[4] != None and sprite.rect.x > cor[4]:
+                        continue
                     if abs(cor[0] - sprite.rect.x) < REND_DIST + 50:
-                        d = ((cor[0] - sprite.rect.x) ** 2 + (cor[1] - sprite.rect.y)  ** 2) ** 0.5
-                        if d < REND_DIST + 50: dist_b.append((d, LIGHT_DIST))
+                        d = (
+                            (cor[0] - sprite.rect.x) ** 2
+                            + (cor[1] - sprite.rect.y) ** 2
+                        ) ** 0.5
+
+                        if d < REND_DIST + 50:
+                            dist_b.append([d, cor[2]])
                 sprite.surf.set_alpha(sprite.check_render(dist_b))
                 screen.blit(sprite.surf, sprite.rect)
-                
-    
+
+        z += 1
+
     for sprite in ui_group:
         screen.blit(sprite.surf, sprite.rect)
         if sprite.type == 1:
-            screen.blit(small_font.render("Pick Lv" + str(player.pickaxe), True, (255, 255, 0)), (sprite.rect.x, sprite.rect.y-20))
+            screen.blit(
+                small_font.render("Pick Lv" + str(player.pickaxe), True, (255, 255, 0)),
+                (sprite.rect.x, sprite.rect.y - 20),
+            )
             if sprite.rect.collidepoint(x, y):
                 player.upgrade_pick()
         if sprite.type == 2:
@@ -186,7 +270,9 @@ while running:
                 screen.blit(ui.surf, ui.rect)
                 if ui.rect.collidepoint(x, y):
                     selected_block = ui.id
-    screen.blit(font.render("Tools and Upgrades", True, (255, 255, 0)), (SCREEN_WIDTH-200, 0))
+    screen.blit(
+        font.render("Tools and Upgrades", True, (255, 255, 0)), (SCREEN_WIDTH - 200, 0)
+    )
     screen.blit(player.surf, player.rect)
     checked_light_blocks = True
 
@@ -195,40 +281,57 @@ while running:
         text = block[0]
         pos = (block[2][0] + 15, block[2][1])
         if selected_block == z:
-            text = '->' + text
+            text = "->" + text
         text += ": " + str(player.inventory[str(z)])
         screen.blit(font.render(text, True, (255, 255, 0)), pos)
         z += 1
-
 
     if is_build:
         screen.blit(font.render("Building...", True, (255, 255, 0)), (30, 10))
     else:
         screen.blit(font.render("Mining...", True, (255, 255, 0)), (30, 10))
 
-
     pygame.display.flip()
     clock.tick(FRAME_RATE)
 
+
 def split(a, n):
     k, m = divmod(len(a), n)
-    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+    return (a[i * k + min(i, m) : (i + 1) * k + min(i + 1, m)] for i in range(n))
 
-with open('stats.json', 'w') as file:
+
+with open("stats.json", "w") as file:
     x_pos = chunk_one.blocks[0].rect.x + 12
     y_pos = chunk_one.blocks[0].rect.y + 12
-    data = {'hp': player.hp, 'pickaxe': player.pickaxe, 'axe': player.axe, 'sword': player.sword, 'x_pos': x_pos, 'y_pos': y_pos}
+    data = {
+        "hp": player.hp,
+        "pickaxe": player.pickaxe,
+        "axe": player.axe,
+        "sword": player.sword,
+        "x_pos": x_pos,
+        "y_pos": y_pos,
+    }
     json.dump(data, file)
 
 
-with open('inventory.json', 'w') as file:
+with open("inventory.json", "w") as file:
     json.dump(player.inventory, file)
 
 
-with open('world.json', 'w') as file:
-    keys = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+with open("world.json", "w") as file:
+    keys = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     z = 0
-    data = {'one': [], 'two': [], 'three': [], 'four': [], 'five': [], 'six': [], 'seven': [], 'eight': [], 'nine': []}
+    data = {
+        "one": [],
+        "two": [],
+        "three": [],
+        "four": [],
+        "five": [],
+        "six": [],
+        "seven": [],
+        "eight": [],
+        "nine": [],
+    }
     for chunk in chunk_list:
         rows = list(split(chunk.blocks, 32))
         for row in rows:
